77956477abe6a3a5b8f28f851ed8c7db05c95630
Kiyan Alav <kiyanalavi1379@gmail.com>
2025-12-05 13:38:20 +0330

    - fix: add safe script - fix: fall back for env

--- template/src/configs/env.ts
+++ template/src/configs/env.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  MONGO_URI: process.env.MONGO_URI || "",
+  MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/mongodbName",
   PORT: process.env.PORT || 5000,
-  ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY || "",
-  REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY || "",
-  BASE_URL: process.env.BASE_URL || "",
+  ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY || "secret",
+  REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY || "secret",
+  BASE_URL: process.env.BASE_URL || "http://localhost:5000",
 };
